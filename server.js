4bba8713c76c42aac0d9e52acfa0423960f5c78f
pranavkarthik009 <mvpranavkarthik.gmail.com>
2025-11-01 20:48:59 +0530

    added connection to mongoDB

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+const { MongoClient }=require('mongodb'); //mongodb module import
 const {createServer} = require('node:http'); //http module import
 require('dotenv').config(); //dotenv module import
 
@@ -7,6 +7,22 @@ const port=3000;
 
 //server creation
 //commit
+// connection to mongodb database
+async function connectToDB(){
+    const uri=process.env.DB_URL;
+    const client=new MongoClient(uri);  
+    try{
+        await  client.connect();
+        console.log('Connected to MongoDB');
+    }
+    catch(err){
+        console.error('Error connecting to MongoDB:', err);
+    }
+    finally{
+         await client.close();
+    }
+}
+connectToDB();
 
 
 const server = createServer((req,res)=>{
